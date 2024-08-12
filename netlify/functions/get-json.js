c98cbd2284361ee0a095dbfecaa8c182e350a1bb
Nathan <nathan.miconi@etu.univ-lyon1.fr>
2024-08-12 10:31:22 +0200

    files modified

--- netlify/functions/get-json.js
+++ netlify/functions/get-json.js
@@ -2,11 +2,10 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async function(event) {
-  const pathParts = event.path.split('/'); 
-  const user = pathParts[1]; 
+  const user = event.queryStringParameters.user;  // Récupération du nom d'utilisateur à partir des paramètres de la requête
 
-  // Construire le chemin absolu au fichier JSON
-  let filePath = path.resolve(__dirname, `./files/to_${user}.json`);
+  // Construction d'un chemin relatif basé sur __dirname (répertoire actuel)
+  let filePath = path.join(__dirname, `../files/to_${user}.json`);
 
   try {
     const data = fs.readFileSync(filePath, 'utf8');
