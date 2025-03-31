27bcc6c342cfd71ef2328d7f91c761d641f7494d
BlackArrowEtu <nathan.miconi@gmail.com>
2025-03-31 16:03:19 +0200

    feat: model with file

--- 3D/3D.js
+++ 3D/3D.js
@@ -34,7 +34,7 @@ scene.add(model);*/
  const loader = new GLTFLoader();
  const scaleFactor = 2; 
  loader.load(
-     'https://portfolio-models-nathan.s3.eu-north-1.amazonaws.com/gold_barrel.glb',
+     '/3D/gold_barrel.glb',
      (gltf) => {
          const loadedModel = gltf.scene;
          
